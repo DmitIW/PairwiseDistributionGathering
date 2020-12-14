5c46782a73716282fbf92b0f6d85164db994272f
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-14 12:02:25 +0300

    added context management to Flow

--- flow/create_flow.py
+++ flow/create_flow.py
@@ -1,23 +1,23 @@
 from flow.clickhouse_agent import (
-    SelectQueryResult
+    ClickhouseAgent
 )
 from processing.storage import (
     Storage
 )
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 
 class Flow:
-    def __init__(self, fr: SelectQueryResult, to: Storage, **kwargs):
-        self.fr = fr
-        self.to = to
+    def __init__(self, clickhouse_agent: ClickhouseAgent, target_storage: Storage, **kwargs):
+        self.ch_agent = clickhouse_agent
+        self.target_storage = target_storage
         self.args = kwargs
 
-    def get_source(self) -> SelectQueryResult:
-        return self.fr
+    def get_source(self) -> ClickhouseAgent:
+        return self.ch_agent
 
     def get_destination(self) -> Storage:
-        return self.to
+        return self.target_storage
 
     def get_argument(self, name: str, default_value: Any = None):
         if name in self.args:
@@ -27,6 +27,15 @@ class Flow:
     def get_arguments(self) -> Dict[str, Any]:
         return self.args
 
+    def __enter__(self) -> Tuple[ClickhouseAgent, Storage]:
+        source = self.ch_agent.__enter__()
+        destination = self.target_storage.__enter__()
+        return source, destination
 
-def create_flow(flow_from: SelectQueryResult, flow_to: Storage, **kwargs) -> Flow:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.ch_agent.__exit__(exc_type, exc_val, exc_tb)
+        self.target_storage.__exit__(exc_type, exc_val, exc_tb)
+
+
+def create_flow(flow_from: ClickhouseAgent, flow_to: Storage, **kwargs) -> Flow:
     return Flow(flow_from, flow_to, **kwargs)
