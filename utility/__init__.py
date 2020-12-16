ec1002e388150882b1d15c6e031e696895d014c0
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-16 17:30:34 +0300

    pretty output current time

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,7 +1,7 @@
 from utility.current_time import (
-    current_time
+    current_time, current_time_str
 )
 
 __all__ = [
-    "current_time"
+    "current_time", "current_time_str"
 ]
--- utility/current_time.py
+++ utility/current_time.py
@@ -1,6 +1,11 @@
 import time
 import math
+import datetime
 
 
 def current_time() -> int:
     return math.floor(time.time())
+
+
+def current_time_str() -> str:
+    return datetime.datetime.now().strftime("%A, %B %d, %I:%M %p")
