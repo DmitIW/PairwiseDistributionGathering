b23c118f80337156cb9215c5c19c6b05b8311447
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-16 18:16:55 +0300

    awaited store

--- processing/flow_processing.py
+++ processing/flow_processing.py
@@ -1,7 +1,7 @@
 from typing import Generator, Any, Callable, NoReturn, Iterator
 from functools import partial
 
-from processing.storage import Storage
+from processing.storage import AStorage, Storage
 
 
 def get_and_store(data_flow: Generator[Any, Any, None], storage: Storage, callback: Callable[..., Any],
@@ -11,6 +11,6 @@ def get_and_store(data_flow: Generator[Any, Any, None], storage: Storage, callba
         storage.store(car_callback(data))
 
 
-async def flow_processing_task(data_flow: Iterator, storage: Storage) -> NoReturn:
+async def flow_processing_task(data_flow: Iterator, storage: AStorage) -> NoReturn:
     for data in data_flow:
-        storage.store(data)
+        await storage.store(data)
