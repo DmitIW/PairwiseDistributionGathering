d115ae7a371452c5f229723a47cd8dee27792264
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-18 18:18:25 +0300

    community labelling algorithm added

---
+++ data_gathering/community_labeling/__init__.py
@@ -0,0 +1,7 @@
+from data_gathering.community_labeling import (
+    algorithm as comm_labelling
+)
+
+__all__ = [
+    "comm_labelling"
+]
---
+++ data_gathering/community_labeling/algorithm.py
@@ -0,0 +1,31 @@
+import numpy as np
+import networkx as nx
+import community as community_louvain
+
+from typing import Generator, Any, Dict, Iterator
+
+
+def get_partition(graph: nx.Graph, delete: bool) -> Dict[str, Any]:
+    result = community_louvain.best_partition(graph)
+    if delete:
+        del graph
+    return result
+
+
+def get_graph(nodes: np.ndarray, delete: bool) -> nx.Graph:
+    if nodes.shape[1] != 3:
+        raise TypeError("Unexpected the number of dimensions for nodes")
+    graph = nx.Graph()
+    graph.add_weighted_edges_from(nodes)
+    if delete:
+        del nodes
+    return graph
+
+
+def gathering_nodes(data_flow: Iterator) -> np.ndarray:
+    return np.array([data for data in data_flow])
+
+
+def algorithm(data_flow: Iterator) -> Generator[Any, Any, None]:
+    for node, comm in get_partition(get_graph(gathering_nodes(data_flow), True), True):
+        yield node, comm
