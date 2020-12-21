358200a0ace56eb934a925abc700ea1aa7293589
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-21 13:05:59 +0300

    added getting minute number since Unix time

--- data_gathering/base/time_utility.py
+++ data_gathering/base/time_utility.py
@@ -7,5 +7,9 @@ def current_time() -> int:
     return math.floor(time.time())
 
 
+def current_unix_minute() -> int:
+    return current_time() // 60
+
+
 def current_time_str() -> str:
     return datetime.datetime.now().strftime("%A, %B %d, %I:%M %p")
